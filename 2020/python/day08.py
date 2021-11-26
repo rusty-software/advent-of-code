4b23d7550a1b7e6b2517960a03c31d714a0358ed
Rusty Bentley <mycoding.alert@gmail.com>
2021-11-25 22:01:38 -0600

    finished part 2

--- 2020/python/day08.py
+++ 2020/python/day08.py
@@ -1,21 +1,11 @@
+import copy
+
 with open("../input/day08.txt", "r") as fp:
     lines = fp.readlines()
     lines = [line.rstrip() for line in lines]
 
 
-def more_valid_instructions(instructions, next_instruction_idx, executed_instruction_idxs):
-    if next_instruction_idx < 0 or next_instruction_idx >= len(instructions):
-        print(f'next_instruction_idx {next_instruction_idx} is out of bounds')
-        return False
-
-    if next_instruction_idx in executed_instruction_idxs:
-        print(f'next_instruction_idx {next_instruction_idx} already executed: {executed_instruction_idxs}')
-        return False
-
-    return True
-
-
-def part1():
+def lines_to_instructions():
     instructions = []
     for line in lines:
         operation, rest = line.split(' ')
@@ -23,10 +13,27 @@ def part1():
         amount = rest[1:]
         instructions.append({'operation': operation, 'direction': direction, 'amount': amount})
 
+    return instructions
+
+
+def more_valid_instructions(instructions, next_instruction_idx, executed_instruction_idxs):
+    if next_instruction_idx >= len(instructions):
+        # print(f'next_instruction_idx {next_instruction_idx} is out of bounds')
+        return 'finished'
+
+    if next_instruction_idx in executed_instruction_idxs:
+        # print(f'next_instruction_idx {next_instruction_idx} already executed: {executed_instruction_idxs}')
+        return 'infinite loop'
+
+    return 'more instructions'
+
+
+def try_instructions(instructions):
     executed_instruction_idxs = []
     next_instruction_idx = 0
     accumulated = 0
-    while more_valid_instructions(instructions, next_instruction_idx, executed_instruction_idxs):
+    instruction_result = 'more instructions'
+    while instruction_result == 'more instructions':
         executed_instruction_idxs.append(next_instruction_idx)
         current_instruction = instructions[next_instruction_idx]
 
@@ -48,11 +55,55 @@ def part1():
                 next_instruction_idx += amount
             else:
                 next_instruction_idx -= amount
+        instruction_result = more_valid_instructions(instructions, next_instruction_idx, executed_instruction_idxs)
 
-    print(f'Accumulated: {accumulated}')
+    return instruction_result, accumulated
+
+
+def part1():
+    instructions = lines_to_instructions()
+    instruction_result, accumulated = try_instructions(instructions)
+
+    print(f'Instructions result: {instruction_result}, accumulated: {accumulated}')
+    return
+
+
+def switch_operation(instructions, op_idx):
+    instruction = instructions[op_idx]
+    operation = instruction.get('operation')
+    if operation == 'nop':
+        instruction.update({'operation': 'jmp'})
+    elif operation == 'jmp':
+        instruction.update({'operation': 'nop'})
+
+    instructions[op_idx] = instruction
+
+    return
+
+
+def part2():
+    instructions = lines_to_instructions()
+    nop_jmp_idxs = []
+    for i, instruction in enumerate(instructions):
+        operation = instruction.get('operation')
+        if operation != 'acc':
+            nop_jmp_idxs.append(i)
+
+    print(f'nop_jmp_idxs_count: {len(nop_jmp_idxs)}')
+    for op_idx in nop_jmp_idxs:
+        new_instructions = copy.deepcopy(instructions)
+        switch_operation(new_instructions, op_idx)
+        instruction_result, accumulated = try_instructions(new_instructions)
+        if instruction_result == 'finished':
+            print(f'Finished successfully! Accumulated: {accumulated}')
+            break
+
+    print('Never managed to finish...')
     return
 
 
 if __name__ == "__main__":
-    part1()
+    # part1()
     # 1200
+    part2()
+    # 1023
