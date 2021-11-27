0ff358f6233b3ef68bdd398ac73af1b926c6af19
Rusty Bentley <mycoding.alert@gmail.com>
2021-11-26 21:19:31 -0600

    i gave up; part2 found on the internet

--- 2020/python/day10.py
+++ 2020/python/day10.py
@@ -22,7 +22,6 @@ def part1():
     gaps = {}
     for num in nums:
         diff = num - last_num
-        print(f'current num: {num}, last num: {last_num}, diff: {diff}')
         if diff not in gaps.keys():
             gaps[diff] = 1
         else:
@@ -30,13 +29,35 @@ def part1():
         last_num = num
 
     gaps[3] += 1
+    print(nums)
     print(gaps)
     print(f'product: {gaps[1]} * {gaps[3]} = {gaps[1] * gaps[3]}')
     return
 
 
+def part2():
+    """
+    Yeah, this is an algorithm that I didn't bother to attempt.
+    Solution once again kindly provided by the Internet.
+    https://dev.to/qviper/advent-of-code-2020-python-solution-day-10-30kd
+    """
+    nums.sort()
+    sol = {0: 1}
+    for num in nums:
+        sol[num] = 0
+        if num - 1 in sol:
+            sol[num] += sol[num - 1]
+        if num - 2 in sol:
+            sol[num] += sol[num - 2]
+        if num - 3 in sol:
+            sol[num] += sol[num - 3]
+
+    print(sol[max(nums)])
+    return
+
+
 if __name__ == "__main__":
     part1()
-    # ?
-    # part2()
+    # 2516
+    part2()
     # ?
