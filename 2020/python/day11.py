3f10c3fbdad872127184a8be18f3ce4858dc320e
Rusty Bentley <mycoding.alert@gmail.com>
2021-11-27 17:20:10 -0600

    it's slow, but part2 of day11 works

--- 2020/python/day11.py
+++ 2020/python/day11.py
@@ -34,18 +34,90 @@ def part1():
                 else:
                     evolved_seats[row_idx][seat_idx] = seat
 
-        occupied_seats_sum = 0
-        for row in evolved_seats:
-            occupied_seats_sum += sum([len(s) for s in row if s == '#'])
-            print(''.join(row))
+    occupied_seats_sum = 0
+    for row in evolved_seats:
+        occupied_seats_sum += sum([len(s) for s in row if s == '#'])
+        print(''.join(row))
 
-        print(f'occupied seats sum: {occupied_seats_sum}')
+    print(f'occupied seats sum: {occupied_seats_sum}')
 
     return
 
 
+def up(n):
+    return n - 1
+
+
+def left(n):
+    return n - 1
+
+
+def down(n):
+    return n + 1
+
+
+def right(n):
+    return n + 1
+
+
+def middle(n):
+    return n
+
+
+def seats_viewable_from(row_idx, seat_idx, seats):
+    viewables = []
+
+    directions = [[up, left], [up, middle], [up, right],
+                  [middle, left], [middle, right],
+                  [down, left], [down, middle], [down, right]]
+
+    for row_fn, seat_fn in directions:
+        r = row_idx
+        s = seat_idx
+        while True:
+            r = row_fn(r)
+            s = seat_fn(s)
+            if r < 0 or s < 0 \
+                    or r >= len(seats) or s >= len(seats[r]):
+                break
+            seat = seats[r][s]
+            if seat == '.':
+                continue
+            else:
+                viewables.append(seat)
+                break
+
+    return viewables
+
+
+def part2():
+    seats = []
+    evolved_seats = initial_seats.copy()
+
+    while seats != evolved_seats:
+        seats = copy.deepcopy(evolved_seats)
+        for row_idx, seat_row in enumerate(seats):
+            for seat_idx, seat in enumerate(seat_row):
+                viewable_seats = seats_viewable_from(row_idx, seat_idx, seats)
+                if seat == 'L' and '#' not in viewable_seats:
+                    evolved_seats[row_idx][seat_idx] = '#'
+                elif seat == '#' and len([s for s in viewable_seats if s == '#']) >= 5:
+                    evolved_seats[row_idx][seat_idx] = 'L'
+                else:
+                    evolved_seats[row_idx][seat_idx] = seat
+
+    occupied_seats_sum = 0
+    for row in evolved_seats:
+        occupied_seats_sum += sum([len(s) for s in row if s == '#'])
+        print(''.join(row))
+    print()
+
+    print(f'occupied seats sum: {occupied_seats_sum}')
+    return
+
+
 if __name__ == "__main__":
-    part1()
+    # part1()
     # 2222
-    # part2()
-    # ?
+    part2()
+    # 2032
