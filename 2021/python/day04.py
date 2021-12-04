ea17add22f4e36fa776f0300a26ca0e90186412a
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-04 12:00:08 -0600

    cleanup; still feel like there's some list comprehension missing

--- 2021/python/day04.py
+++ 2021/python/day04.py
@@ -2,6 +2,23 @@ with open("../input/day04.txt", "r") as fp:
     lines = [line.rstrip() for line in fp.readlines()]
 
 
+def parse_lines():
+    hopper = []
+    cards = []
+    for row_idx, row in enumerate(lines):
+        if row_idx == 0:
+            hopper = row.split(',')
+            continue
+
+        if row == '':
+            cards.append([])
+            continue
+        else:
+            card_idx = len(cards) - 1
+            cards[card_idx].append([(row[i:i + 3]).strip() for i in range(0, len(row), 3)])
+    return hopper, cards
+
+
 def find_num_pos_in_card(card, num):
     """
     Given a card and num, returns a list of the row and col indexes if the num is in the card, None otherwise.
@@ -12,7 +29,7 @@ def find_num_pos_in_card(card, num):
     return None
 
 
-def bingo(found_nums):
+def bingo_check(found_nums):
     """
     Given a list of coordinates, returns a list of coordinates if all the coordinates in a row or column are present,
     otherwise None.
@@ -24,35 +41,23 @@ def bingo(found_nums):
             print(f'bingo in row: {covered_rows}')
             return covered_rows
         if len(covered_cols) == 5:
-            print(f'bingo in col: {covered_rows}')
+            print(f'bingo in col: {covered_cols}')
             return covered_cols
     return False
 
 
 def part1():
-    hopper = []
-    cards = []
-    for row_idx, row in enumerate(lines):
-        if row_idx == 0:
-            hopper = row
-            continue
-
-        if row == '':
-            cards.append([])
-            continue
-        else:
-            card_idx = len(cards) - 1
-            cards[card_idx].append([(row[i:i + 3]).strip() for i in range(0, len(row), 3)])
+    hopper, cards = parse_lines()
 
     marked_nums = dict()
-    for num in hopper.split(','):
+    for num in hopper:
         for card_idx, card in enumerate(cards):
             num_pos = find_num_pos_in_card(card, num)
             if num_pos:
                 if card_idx not in marked_nums.keys():
                     marked_nums[card_idx] = []
                 marked_nums[card_idx].append(num_pos)
-                bingo_coords = bingo(marked_nums[card_idx])
+                bingo_coords = bingo_check(marked_nums[card_idx])
                 if bingo_coords:
                     total = 0
                     for row_idx, row in enumerate(card):
