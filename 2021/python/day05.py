c8c6b1057b60d70bf9b64a6959f05e97df303874
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-05 09:56:15 -0600

    day05 part 2 finished

--- 2021/python/day05.py
+++ 2021/python/day05.py
@@ -3,10 +3,17 @@ with open("../input/day05.txt", "r") as fp:
 
 
 def string_to_point(string_point):
+    """
+    Given a string point, returns a list of integers representing the point in the form [x, y].
+    """
     return [int(x) for x in string_point.split(',')]
 
 
 def endpoints_to_points(var_start, var_end, const_idx, const_idx_val):
+    """
+    Given a start value, end value, constant index, and constant value, returns a list of all integer points between
+    the start and end.
+    """
     line = []
     rs = min([var_start, var_end])
     re = max([var_start, var_end])
@@ -19,7 +26,30 @@ def endpoints_to_points(var_start, var_end, const_idx, const_idx_val):
     return line
 
 
-def line_to_points(unparsed_data):
+def endpoints_to_diagonal(start, end):
+    """
+    Given a start and end point, returns a list of all integer points between them. Assumes a 45 degree diagonal.
+    """
+    start_point = min(start, end)
+    end_point = max(start, end)
+    x_range = end_point[0] - start_point[0]
+    y_incrementing = end_point[1] > start_point[1]
+    points = []
+    for x in range(x_range):
+        if y_incrementing:
+            points.append([start_point[0] + x, start_point[1] + x])
+        else:
+            points.append([start_point[0] + x, start_point[1] - x])
+
+    points.append(end_point)
+    return points
+
+
+def line_to_points(unparsed_data, include_diagonals=False):
+    """
+    Given unparsed data, parses the data and returns the list of integer points between the start and end of the points
+    defining the line. Only processes horizontal and vertical lines by default, and can return None.
+    """
     start, end = unparsed_data.split(' -> ')
     start = string_to_point(start)
     end = string_to_point(end)
@@ -27,11 +57,16 @@ def line_to_points(unparsed_data):
         return endpoints_to_points(start[1], end[1], 0, start[0])
     elif start[1] == end[1]:
         return endpoints_to_points(start[0], end[0], 1, start[1])
+    elif include_diagonals:
+        return endpoints_to_diagonal(start, end)
 
     return None
 
 
 def increment_touches(point_touches, points):
+    """
+    Given a dictionary of points to touch counts and a list of points, adds the point touches to the dictionary.
+    """
     for p in points:
         point = tuple(p)
         if point in point_touches:
@@ -43,6 +78,10 @@ def increment_touches(point_touches, points):
 
 
 def count_gte_touches(point_touches, threshold):
+    """
+    Given a dictionary of points to touch counts and a threshold, counts the number of points greater than or equal
+    to the threshold.
+    """
     keep_count = 0
     for k, v in point_touches.items():
         if v >= threshold:
@@ -51,10 +90,10 @@ def count_gte_touches(point_touches, threshold):
     return keep_count
 
 
-def part1():
+def part1(include_diagonals=False):
     point_touches = dict()
     for line in lines:
-        points = line_to_points(line)
+        points = line_to_points(line, include_diagonals)
         if points:
             increment_touches(point_touches, points)
 
@@ -63,8 +102,13 @@ def part1():
     return
 
 
+def part2():
+    part1(True)
+    return
+
+
 if __name__ == "__main__":
     part1()
     # 7269
-    # part2()
-    #
+    part2()
+    # 21140
