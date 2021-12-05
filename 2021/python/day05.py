7b43a3dcbe6b2b077b880487a180d57d4b2ef9c1
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-05 11:32:04 -0600

    adds perf timing

--- 2021/python/day05.py
+++ 2021/python/day05.py
@@ -1,3 +1,5 @@
+import time
+
 with open("../input/day05.txt", "r") as fp:
     lines = [line.rstrip() for line in fp.readlines()]
 
@@ -107,8 +109,16 @@ def part2():
     return
 
 
+def with_perf_timing(fn):
+    start_time = time.perf_counter()
+    fn()
+    end_time = time.perf_counter()
+    print(f'Execution time: {end_time - start_time:0.6f}')
+
+
 if __name__ == "__main__":
-    part1()
+    with_perf_timing(part1)
     # 7269
-    part2()
+
+    with_perf_timing(part2)
     # 21140
