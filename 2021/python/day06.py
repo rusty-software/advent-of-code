eab4f9c7d0f960d383cf7d542ac003a3e45e5773
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-06 15:04:27 -0600

    day06 part 2 finished; approach makes a difference...

--- 2021/python/day06.py
+++ 2021/python/day06.py
@@ -22,6 +22,31 @@ def population_growth(initial_population, days):
     return current_population
 
 
+def organized_population_growth(initial_population, days, reproduction_frequency):
+    current_population = initial_population.copy()
+
+    population_age_counts = dict()
+    for age_key in range(reproduction_frequency + 2):
+        population_age_counts[age_key] = 0
+
+    for age in current_population:
+        population_age_counts[age] += 1
+
+    for day in range(days):
+        working = population_age_counts.copy()
+
+        for age_key, age_count in working.items():
+            if age_key == 0:
+                population_age_counts[reproduction_frequency + 1] = age_count
+                population_age_counts[reproduction_frequency - 1] = age_count + working[reproduction_frequency]
+            elif age_key == reproduction_frequency:
+                continue
+            else:
+                population_age_counts[age_key - 1] = working[age_key]
+
+    return population_age_counts
+
+
 def part1():
     initial_population = line
     grown_population = population_growth(initial_population, 80)
@@ -32,14 +57,14 @@ def part1():
 
 def part2():
     initial_population = line
-    grown_population = population_growth(initial_population, 256)
-    print(f'population count after 256 days: {len(grown_population)}')
+    grown_population = organized_population_growth(initial_population, 256, 7)
+    print(f'population count after 256 days: {sum(grown_population.values())}')
 
     return
 
 
 if __name__ == "__main__":
-    # with_perf_timing(part1)
+    with_perf_timing(part1)
     # 372984
     with_perf_timing(part2)
-    #
+    # 1681503251694
