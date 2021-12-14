f5453b20c1c485a976d115c8f5f423a9e0ae3ca7
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-14 14:42:01 -0600

    day14 part 2 finished

--- 2021/python/day14.py
+++ 2021/python/day14.py
@@ -1,5 +1,7 @@
 import time
 from collections import Counter
+from copy import deepcopy
+from pprint import pprint
 
 with open("../input/day14.txt", "r") as fp:
     file_data = [line.rstrip() for line in fp.readlines()]
@@ -41,11 +43,11 @@ def grow(polymer, rules):
     return ''.join(grown_polymer)
 
 
-def part1():
+def grow_by_iteration(iteration_count):
     polymer, unparsed_rules = to_template_rules(file_data)
     rules = to_rules(unparsed_rules)
 
-    for i in range(10):
+    for i in range(iteration_count):
         polymer = grow(polymer, rules)
     frequency = Counter(polymer)
     most = max(frequency, key=frequency.get)
@@ -56,8 +58,67 @@ def part1():
     return
 
 
+def part1():
+    grow_by_iteration(10)
+
+
+def to_pair_data(unparsed_rules):
+    rules = dict()
+    for pair, product in [r.split(' -> ') for r in unparsed_rules]:
+        rules[pair] = {'product': product, 'count': 0}
+    return rules
+
+
+def initialize_pair_count(pairs, polymer):
+    for idx, element in enumerate(polymer):
+        if idx == len(polymer) - 1:
+            break
+        pairs[f'{element}{polymer[idx + 1]}']['count'] += 1
+    return
+
+
+def initialize_product_count(product_count, pairs, polymer):
+    for pair, pair_data in pairs.items():
+        product = pair_data['product']
+        if product not in product_count:
+            product_count[product] = 0
+
+    for element in polymer:
+        product_count[element] += 1
+    return
+
+
+def part2():
+    # apparently, brute force won't scale to 40
+    polymer, unparsed_rules = to_template_rules(file_data)
+    pairs = to_pair_data(unparsed_rules)
+    initialize_pair_count(pairs, polymer)
+    product_counts = dict()
+    initialize_product_count(product_counts, pairs, polymer)
+
+    working_pairs = deepcopy(pairs)
+    for i in range(40):
+        for pair, pair_data in working_pairs.items():
+            pair_count = pair_data['count']
+            if pair_count > 0:
+                product = pair_data['product']
+                left_pair = f'{pair[0]}{product}'
+                right_pair = f'{product}{pair[1]}'
+                pairs[left_pair]['count'] += pair_count
+                pairs[right_pair]['count'] += pair_count
+                pairs[pair]['count'] -= pair_count
+                product_counts[product] += pair_count
+        working_pairs = deepcopy(pairs)
+    pprint(pairs)
+    pprint(product_counts)
+    max_count = max([c for c in product_counts.values()])
+    min_count = min([c for c in product_counts.values()])
+    print(max_count - min_count)
+    return
+
+
 if __name__ == "__main__":
-    with_perf_timing(part1)
+    # with_perf_timing(part1)
     # 2549
-    # with_perf_timing(part2)
+    with_perf_timing(part2)
     #
