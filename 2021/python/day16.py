faf2f8152ea51014282bc9fbabc13c9e99ca2b09
Rusty Bentley <mycoding.alert@gmail.com>
2021-12-16 18:55:12 -0600

    I feel like this is doable, but also a lot of work.

--- 2021/python/day16.py
+++ 2021/python/day16.py
@@ -34,13 +34,19 @@ hex_to_bits = {'0': '0000',
 length_types = {0: 15,
                 1: 11}
 
+packet_type_literal = 4
+
 
 def hex_string_to_bits(hex_string):
     return ''.join([hex_to_bits.get(c) for c in hex_string])
 
 
-def bits_to_int(bits):
-    return int(bits, 2)
+def bits_to_int(bits, s):
+    return int(bits, 2), s[len(bits):]
+
+
+def bits_and_remaining(bits, s):
+    return bits, s[len(bits):]
 
 
 def initial_assertions():
@@ -54,44 +60,71 @@ def initial_assertions():
                                      'version': 7,
                                      'type_id': 3}}
     for hex_string, expected in assertions.items():
-        bits = hex_string_to_bits(hex_string)
-        assert expected.get('bits') == bits
-        assert expected.get('version') == bits_to_int(bits[:3])
-        assert expected.get('type_id') == bits_to_int(bits[3:6])
+        original_bits = hex_string_to_bits(hex_string)
+        bits = original_bits
+        version, remaining_bits = bits_to_int(bits[:3], bits)
+        type_id, remaining_bits = bits_to_int(remaining_bits[:3], remaining_bits)
+        assert expected.get('bits') == original_bits
+        assert expected.get('version') == version
+        assert expected.get('type_id') == type_id
 
     return
-
-
-PACKET_TYPE_LITERAL = 4
 
 
 def part1():
     initial_assertions()
 
+    literals = []
     # python could probably do the translation for us, but let's do it ourselves, like santa intended
+    # bits = hex_string_to_bits('D2FE28')
     bits = hex_string_to_bits('38006F45291200')
-    version, type_id = bits_to_int(bits[:3]), bits_to_int(bits[3:6])
-    if type_id != PACKET_TYPE_LITERAL:
-        length_type_id = int(bits[6])
-        start_idx = 7
-        end_idx = start_idx + length_types.get(length_type_id)
-
-        sub_packet_bit_length = int(bits[start_idx:end_idx], 2)
-        start_idx = end_idx
-        end_idx = start_idx + sub_packet_bit_length
-        sub_packet = bits[start_idx:end_idx]
-
-        sub_packet_version, sub_packet_type_id = bits_to_int(sub_packet[:3]), bits_to_int(sub_packet[3:6])
-        if sub_packet_type_id == PACKET_TYPE_LITERAL:
+    version, remaining_bits = bits_to_int(bits[:3], bits)
+    type_id, remaining_bits = bits_to_int(remaining_bits[:3], remaining_bits)
+    print(f'version: {version}, type_id: {type_id}')
+    while len(remaining_bits) > 0:
+        print(f'continuing to process: (length: {len(remaining_bits)}) {remaining_bits}')
+        if type_id == packet_type_literal:
             continuation_character_found = True
-            digits = []
             while continuation_character_found:
-                continuation_character = int(sub_packet[6])
-                continuation_character_found = continuation_character == 1
-                digits.append(int(sub_packet[7:11], 2))
-            print(digits)
+                continuation_character, remaining_bits = bits_and_remaining(remaining_bits[0], remaining_bits)
+                # print(f'continuation: {continuation_character}, remaining: (length: {len(remaining_bits)}) {remaining_bits}')
+                continuation_character_found = int(continuation_character) == 1
+                # print(f'continuation character found? {continuation_character_found}')
+                literal, remaining_bits = bits_and_remaining(remaining_bits[:4], remaining_bits)
+                # print(f'appending {literal} to {literals}')
+                literals.append(literal)
+                if not continuation_character_found:
+                    remaining_bits = ''
+        else:
+            # operator of some kind
+            length_type_id, remaining_bits = bits_and_remaining(bits[0], bits)
+            print(f'length_type_id:{length_type_id}')
+            remaining_bits = ''
 
-    print(f'bits: {bits}, version: {version}, type_id: {type_id}')
+    print(f'literals:{literals}')
+
+    # version, type_id = bits_to_int(bits[:3]), bits_to_int(bits[3:6])
+    # if type_id != packet_type_literal:
+    #     length_type_id = int(bits[6])
+    #     start_idx = 7
+    #     end_idx = start_idx + length_types.get(length_type_id)
+    #
+    #     sub_packet_bit_length = int(bits[start_idx:end_idx], 2)
+    #     start_idx = end_idx
+    #     end_idx = start_idx + sub_packet_bit_length
+    #     sub_packet = bits[start_idx:end_idx]
+    #
+    #     sub_packet_version, sub_packet_type_id = bits_to_int(sub_packet[:3]), bits_to_int(sub_packet[3:6])
+    #     if sub_packet_type_id == packet_type_literal:
+    #         continuation_character_found = true
+    #         digits = []
+    #         while continuation_character_found:
+    #             continuation_character = int(sub_packet[6])
+    #             continuation_character_found = continuation_character == 1
+    #             digits.append(int(sub_packet[7:11], 2))
+    #         print(digits)
+    #
+    # print(f'bits: {bits}, version: {version}, type_id: {type_id}')
     return
 
 
